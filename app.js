856ee6479f971947ad0b4cbe48a0e2f03743b5ef
eechern Desktop <valenciaracer@gmail.com>
2025-08-03 16:11:06 +0800

    preapred app for deloyment

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -69,9 +70,11 @@ app.use(
   })
 );
 
+app.use(compression());
+
 //Test Middleware
 app.use((req, res, next) => {
-  console.log('hello from the middlewear');
+  // console.log('hello from the middlewear');
   next();
 });
 
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -8,9 +8,7 @@ export const bookTour = async (tourId) => {
       'pk_test_51RqFAMLclz1AZ5Lx5lyFFjKmrRw59ZPbt2Sb6R0zqdZcfYBzfj4IzFDzeXjqwHQ4CLlSGnzJ9BPCkVFLg1ti65mF00KpOBlfPb'
     );
     //1) Get checkout session from API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     console.log(session, stripe);
     //2) Create checkout form + charge credit card
     await stripe.redirectToCheckout({
